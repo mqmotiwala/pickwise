15a67eb7e57ff7073439048610f1a65fb8bfa857
Mufaddal Motiwala <mqmotiwala@gmail.com>
2025-09-14 01:25:21 -0400

    update trades json path to mounted mongodb volume

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ NUM_DAYS_PRECEDING_ANALYSIS = 30
 DATES_FORMAT = "%Y-%m-%d"
 STOCK_PORTFOLIO_COL_NAME = "portfolio_value"
 MARKET_PORTFOLIO_COL_NAME = "market_value"
-TRADES_JSON_PATH = "trades.json"
+TRADES_JSON_PATH = "/data/db/trades.json"
 DEFAULT_TRADES = [{
         "ticker":"AMZN",
         "date":"2025-04-01",
